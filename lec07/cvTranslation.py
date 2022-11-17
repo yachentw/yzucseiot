c2f1ccec297f012cef9f7f42132a6b681ac4decf
cya <yachen.tw@gmail.com>
2022-11-17 23:09:47 +0800

    affine

---
+++ lec07/cvTranslation.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('lena.jpg')
+rows, cols = img.shape[:2]
+M = np.float32([ [1,0,100], [0,1,50] ])
+translation = cv2.warpAffine(img, M, (cols, rows))
+cv2.imshow('Translation', translation)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
