4fe5bec66242eaf4623ce6d9c77edfbab7fc01a4
cya <yachen.tw@gmail.com>
2023-10-05 10:02:16 +0800

    20231005 - lec03

--- lec03/blink2.py
+++ lec03/blink2.py
@@ -5,16 +5,17 @@ LED_PINS = [12, 11]
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED_PINS[0], GPIO.OUT)
 GPIO.setup(LED_PINS[1], GPIO.OUT)
+ledNum = len(LED_PINS)
 try:
     counter = 1
     while True:
-        for i in range(2):
+        for i in range(ledNum):
             if (counter >> i) & 0x00000001:
                 GPIO.output(LED_PINS[i], GPIO.HIGH)
             else:
                 GPIO.output(LED_PINS[i], GPIO.LOW)        
         counter = counter << 1
-        if counter > 2:
+        if counter > ledNum:
             counter = 1
         time.sleep(1)
 except KeyboardInterrupt:
