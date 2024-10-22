d6561d17c1effd3e69d6caf0bf5a15ff6a51d9ec
cya <yachen.tw@gmail.com>
2024-10-23 00:04:15 +0800

    blynk

--- lec06/blynk_get.py
+++ lec06/blynk_get.py
@@ -1,3 +1,4 @@
+import time
 import requests
 
 # Replace with your token
@@ -9,16 +10,18 @@ BLYNK_GET_URL = f'https://blynk.cloud/external/api/get?token={BLYNK_AUTH_TOKEN}&
 
 def get_virtual_pin_value():
     """Get the lastest value of a virtual pin"""
-    try:
-        response = requests.get(BLYNK_GET_URL)
-        if response.status_code == 200:
-            value = response.text.strip()
-            print(f"Virtual PIN {VIRTUAL_PIN}: {value}")
-            return value
-        else:
-            print(f"Cannot access: {response.status_code}")
-    except Exception as e:
-        print(f"Exception: {e}")
+    while True:
+        try:
+            response = requests.get(BLYNK_GET_URL)
+            if response.status_code == 200:
+                value = response.text.strip()
+                print(f"Virtual PIN {VIRTUAL_PIN}: {value}")
+                return value
+            else:
+                print(f"Cannot access: {response.status_code}")
+        except Exception as e:
+            print(f"Exception: {e}")
+        time.sleep(10)
 
 if __name__ == '__main__':
     if BLYNK_AUTH_TOKEN == "...":
