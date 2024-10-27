640cf7c9f8e4bf0e1254a17ae195042230eadbf3
cya <yachen.tw@gmail.com>
2024-10-27 17:08:34 +0800

    cvCrop

--- lec07/cvCrop.py
+++ lec07/cvCrop.py
@@ -3,10 +3,10 @@ import cv2
 img = cv2.imread("cat.jpg")
 cv2.imshow("Normal", img)
 cv2.waitKey(0)
-face = img[90:240, 125:225]
-cv2.imshow("Face", face)
-cv2.waitKey(0)
-body = img[20:, 40:240]
+body = img[53:, 89:]
 cv2.imshow("Body", body)
+cv2.waitKey(0)
+face = img[68:233, 97:231]
+cv2.imshow("Face", face)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
