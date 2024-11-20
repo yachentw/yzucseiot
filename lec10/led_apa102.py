27802ccfd53ffd003bb9c0a50832770a939cf37a
cya <yachen.tw@gmail.com>
2024-11-20 16:11:06 +0800

    apa102

--- lec10/led_apa102.py
+++ lec10/led_apa102.py
@@ -1,12 +1,15 @@
 import apa102
 import time
+import random
+
 LED_NUM = 3
 leds = apa102.APA102(num_led=3)
 colors = [[255,0,0],[0,255,0],[0,0,255]] # LED0: R, LED1: G, LED2: B
 try:
     while True:
         for i in range(LED_NUM):
-            leds.set_pixel(i, colors[i][0], colors[i][1], colors[i][2], 10)
+            r = random.randint(0, 2)
+            leds.set_pixel(i, colors[r][0], colors[r][1], colors[r][2], 10)
         leds.show()
         time.sleep(1)
         leds.clear_strip()
